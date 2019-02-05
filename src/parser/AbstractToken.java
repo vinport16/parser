d4d15768a8bb9bd60033529ec4ee15ddc290af88
Jason Shin <jasonshin98@gmail.com>
2019-02-04 20:33:42 -0500

    gitignore

--- src/parser/AbstractToken.java
+++ src/parser/AbstractToken.java
@@ -2,15 +2,8 @@ package parser;
 
 public abstract class AbstractToken implements Token {
 
-	private TerminalSymbol type;
-
-	// Getter method for type
-	public TerminalSymbol getType() {
-		return type;
-	}
-
 	// Checks to see if type matches the input type
 	public final boolean matches(TerminalSymbol type) {
-		return (this.type == type);
+		return (getType() == type);
 	}
 }
