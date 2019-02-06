33158d73b67734ba8ef205b1d158e7ae82f1aa38
Vincent Portelli <vinport16@gmail.com>
2019-02-06 11:58:13 -0500

    I think ParseState is ready to go

---
+++ src/parser/ParseState.java
@@ -0,0 +1,40 @@
+package parser;
+
+import java.util.*;
+import java.util.stream.*;
+
+final class ParseState {
+	
+	private final boolean success;
+	private final Node node;
+	private final List<Token> remainder;
+	final static ParseState FAILURE = new ParseState(false,null,null);
+	
+	
+	private ParseState(boolean success, Node node, List<Token> remainder){
+		this.success = success;
+		this.node = node;
+		this.remainder = remainder;
+	}
+	
+	public ParseState build(Node node, List<Token> remainder){
+		if(node == null || remainder == null) {
+			throw new NullPointerException("Cannot build ParseState with null parameters");
+		}else {
+			return new ParseState(true, node, remainder.stream().collect(Collectors.toList()));
+		}
+	}
+	
+	public boolean getSuccess(){
+		return success;
+	}
+	
+	public Node getNode(){
+		return node.copy();
+	}
+	
+	public List<Token> getRemainder(){
+		return remainder.stream().collect(Collectors.toList());
+	}
+	
+}
